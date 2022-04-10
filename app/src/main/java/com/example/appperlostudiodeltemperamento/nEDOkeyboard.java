6db0a564df33e91d1cc1d42a7e793e785c5a513c
Riccardo Passoni <riccardopassoni@protonmail.com>
2022-04-10 17:05:51 +0200

    created new branch to test programmatically generated buttons

---
+++ app/src/main/java/com/example/appperlostudiodeltemperamento/nEDOkeyboard.java
@@ -0,0 +1,14 @@
+package com.example.appperlostudiodeltemperamento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nEDOkeyboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nedokeyboard);
+    }
+}
