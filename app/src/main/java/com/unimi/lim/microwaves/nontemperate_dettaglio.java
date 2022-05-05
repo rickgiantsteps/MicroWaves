366f95d0a06240abc917571e4178b7c42c7b0dec
Riccardo Passoni <riccardopassoni@protonmail.com>
2022-05-05 12:27:43 +0200

    renamed all remaining packages and themes, fixed gradle

---
+++ app/src/main/java/com/unimi/lim/microwaves/nontemperate_dettaglio.java
@@ -0,0 +1,28 @@
+package com.unimi.lim.microwaves;
+
+import android.os.Bundle;
+
+import com.unimi.lim.microwaves.ui.main.SectionsPagerAdapter2;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.unimi.lim.microwaves.databinding.ActivityNontemperateDettaglioBinding;
+
+public class nontemperate_dettaglio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.unimi.lim.microwaves.databinding.ActivityNontemperateDettaglioBinding binding = ActivityNontemperateDettaglioBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter2 sectionsPagerAdapter = new SectionsPagerAdapter2(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/unimi/lim/microwaves/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,63 @@
+package com.unimi.lim.microwaves.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.unimi.lim.microwaves.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3, R.string.tab_text_4, R.string.tab_text_5};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch(position) {
+            case 0:
+                return EDO12Fragment.newInstance("", "");
+            case 1:
+                return EDO24Fragment.newInstance("", "");
+            case 2:
+                return EDO22Fragment.newInstance("", "");
+            case 3:
+                return EDO11Fragment.newInstance("", "");
+            case 4:
+                return EDO7Fragment.newInstance("", "");
+            default:
+                return PlaceholderFragment.newInstance(position + 1);
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 5;
+    }
+}
