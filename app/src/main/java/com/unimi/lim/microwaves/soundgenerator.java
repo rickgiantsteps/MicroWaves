886bc84a8928e816560adabc441e95d999861157
Riccardo Passoni <riccardopassoni@protonmail.com>
2022-05-11 22:09:02 +0200

    small soundgenerator improvements

--- app/src/main/java/com/unimi/lim/microwaves/soundgenerator.java
+++ app/src/main/java/com/unimi/lim/microwaves/soundgenerator.java
@@ -8,6 +8,7 @@ import android.media.AudioTrack;
 import android.widget.Toast;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
 public class soundgenerator {
@@ -16,10 +17,15 @@ public class soundgenerator {
     static AudioTrack generateTone(double freq, double volume, int wave, Context context, AudioTrack[] tones, int tonenumber)
     {
 
+        int samplingfreq = 44100;
+
         Toast toast;
 
-        if (freq > 44100.0 / 2) {
-            toast = Toast.makeText(context, "Frequency played: " + new BigDecimal(freq).setScale(4, BigDecimal.ROUND_FLOOR) + " Hz\n\nThe pitch generated is greater than half of the sampling frequency (44100Hz): aliasing might be present", Toast.LENGTH_LONG);
+        if (freq > samplingfreq / 2.0) {
+            toast = Toast.makeText(context, "Frequency played: " + new BigDecimal(freq).setScale(4, RoundingMode.FLOOR) + " Hz\n\nThe pitch generated is greater than half of the sampling frequency (44100Hz): aliasing might be present", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (wave != 0 && freq > 10000) {
+            toast = Toast.makeText(context, "Waves are synthesized by using a 'naive sampling' method, non sine waves can be inaccurate at high frequencies: aliasing might be present", Toast.LENGTH_LONG);
             toast.show();
         }
 
@@ -29,16 +35,16 @@ public class soundgenerator {
             volume = 1; //max volume = 1 regardless of input
         }
 
-        int count = (int)(44100.0 * 2.0 * (durationmillsec / 1000.0)) & ~1;
+        int count = (int)(samplingfreq * 2.0 * (durationmillsec / 1000.0)) & ~1;
         short[] samples = new short[count];
 
         for(int i = 0; i < count; i += 2) {
 
-            double sinewave = Math.sin(2 * Math.PI * i / (44100.0 * 2.0 / freq));
+            double sinewave = Math.sin(2 * Math.PI * i / (samplingfreq * 2.0 / freq));
 
             if (wave == 0){
                 //sine wave
-                short sample = (short)((volume * sinewave) * 0x7FFF);
+                short sample = (short) ((volume * sinewave) * 0x7FFF);
                 //makes a stereo signal
                 samples[i] = sample;
                 samples[i + 1] = sample;
@@ -46,7 +52,7 @@ public class soundgenerator {
             } else if (wave == 1) {
 
                 //this makes a stereo square wave
-                short sample = (short)(sinewave * 0x7FFF);
+                short sample = (short) (sinewave * 0x7FFF);
                 if (sample > 0.0) {
                     samples[i] = (short) (volume/2 * 0x7FFF);
                     samples[i + 1] = (short) (volume/2  * 0x7FFF);
@@ -60,14 +66,14 @@ public class soundgenerator {
             } else if (wave == 2) {
 
                 //this makes a stereo triangle wave
-                short sample = (short)(volume * ((Math.acos(sinewave)/1.5708)-1) * 0x7FFF);
+                short sample = (short) (volume * ((Math.acos(sinewave)/1.5708)-1) * 0x7FFF);
                 samples[i] = sample;
                 samples[i + 1] = sample;
 
             } else if (wave == 3) {
 
                 //this makes a stereo sawtooth wave
-                short sample =(short) (volume * 2*(i%(44100.0 * 2.0 / freq)/(44100.0 * 2.0 / freq)-1) * 0x7FFF);
+                short sample =(short) (volume * 2*(i%(samplingfreq * 2.0 / freq)/(samplingfreq * 2.0 / freq)-1) * 0x7FFF);
                 samples[i] = sample;
                 samples[i + 1] = sample;
 
@@ -82,7 +88,7 @@ public class soundgenerator {
                         .build())
                 .setAudioFormat(new AudioFormat.Builder()
                         .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                        .setSampleRate(44100)
+                        .setSampleRate(samplingfreq)
                         .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
                         .build())
                 .setBufferSizeInBytes(count * (Short.SIZE / 8))
